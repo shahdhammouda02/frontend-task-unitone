1d227fd8e98a1893ea17b89d8aec18efa47256eb
Shahd Hammouda <99508416+shahdhammouda02@users.noreply.github.com>
2023-07-19 23:33:12 +0300

    Update main.js

--- Frontend task/JS/main.js
+++ Frontend task/JS/main.js
@@ -18,3 +18,26 @@ window.onscroll=()=>{
   this.scrollY > 20 ? navbar.classList.add('sticky') : navbar.classList.remove('sticky')
 
 }
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  // margin: 20,
+  autoplay: true,
+  rtl: true,
+  autoplayTimeout: 2000,
+  autoplaySpeed: 2000,
+  slideTransition: "linear",
+  autoplayHoverPause: false,
+  dots: true,
+  responsive: {
+      0: {
+          items: 1,
+      },
+      575: {
+          items: 3,
+      },
+      1000: {
+          items: 5,
+      },
+  },
+});
